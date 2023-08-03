394c2a7a310e5771ee32935d9a0b23a601bd67d3
M0412 <133132845+M0412@users.noreply.github.com>
2023-08-03 12:25:47 -0500

    adding table schema

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,50 @@
+-- Create the schema for the tables
+
+-- Create titles table
+CREATE TABLE titles(
+	title_id VARCHAR(5) NOT NULL PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
+);
+
+-- Create employees table
+CREATE TABLE employees(
+	emp_no INT NOT NULL PRIMARY KEY, 
+	emp_title_id VARCHAR(5) NOT NULL, 
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL, 
+	last_name VARCHAR(30) NOT NULL,
+	sex CHAR(1) NOT NULL, 
+	hire_date DATE NOT NULL, 
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+-- Create salaries table
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+-- Create departments table
+CREATE TABLE departments(
+	dept_no VARCHAR(5) NOT NULL PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL, 
+	dept_no VARCHAR(5) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(5) NOT NULL, 
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
